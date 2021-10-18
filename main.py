1f363f5ddbc171d4bc1eb652c51e17cd7d730a0a
yux829 <yux829@sina.com>
2021-10-18 14:12:38 +0800

    增加巨鲸地址

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#-*- coding:utf-8 -*-
 import PySimpleGUI as sg
 from PySimpleGUI.PySimpleGUI import InputText
 import gettokenholdings
@@ -8,7 +9,7 @@ def main():
     name = 'ParaFi'
     data=[]
     header_list = ['序号', '币种', '数量', '单价($)', '持有价值($)','占比']
-    layout = [[sg.T('*查询地址:',size=(10,1)),sg.Multiline(key='key_address',size=(70,2),default_text=''),sg.Button('查询')],[sg.T('cookie:',size=(10,1)),sg.Multiline(key='key_cookie',size=(70,3),default_text='')],[sg.T('代理服务器:',size=(10,1)),sg.InputText(key='key_proxy',size=(70,2),default_text='')],[sg.InputText(default_text='备注：多个地址以分号分割,从https://cn.etherscan.com/tokenholdings?a=0x5028d77b91a3754fb38b2fbb726af02d1fe44db6 获取cookie',font=("微软雅黑", 10),justification='left',readonly = True,size=(120,1))],[sg.Table(key='key_results',values=data,
+    layout = [[sg.T('*查询地址:',size=(10,1)),sg.Multiline(key='key_address',size=(70,2),default_text="0x5028d77b91a3754fb38b2fbb726af02d1fe44db6;0x4655b7ad0b5f5bacb9cf960bbffceb3f0e51f363;0xd9b012a168fb6c1b71c24db8cee1a256b3caa2a2"),sg.Button('查询')],[sg.T('cookie:',size=(10,1)),sg.Multiline(key='key_cookie',size=(70,3),default_text='')],[sg.T('代理服务器:',size=(10,1)),sg.InputText(key='key_proxy',size=(70,2),default_text='')],[sg.InputText(default_text='备注：多个地址以分号分割,从https://cn.etherscan.com/tokenholdings?a=0x5028d77b91a3754fb38b2fbb726af02d1fe44db6 获取cookie',font=("微软雅黑", 10),justification='left',readonly = True,size=(120,1))],[sg.Table(key='key_results',values=data,
                         headings=header_list,
                         auto_size_columns=False,
                         col_widths=[5,10, 10, 20, 30],
